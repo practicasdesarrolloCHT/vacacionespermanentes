29b54ba8d9c5923c61530b2e0d0ffdd081bde0c5
ruizdiazchristiand <ruizdiazchristiand@gmail.com>
2015-04-17 03:47:06 -0300

    merge con los cambios de Tatiana

--- flapper-news/models/Viajes.js
+++ flapper-news/models/Viajes.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose');
 var ViajeSchema = new mongoose.Schema({
   nombre: String,
   fecha_inicio: Date,
-  fecha_fin: Date
+  fecha_fin: Date,
+  destino: String
 });
 
 
