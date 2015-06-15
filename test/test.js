eacf4e6927addc1c46b46438fceb0741185292f5
Tatiana <tatianamolinari90@gmail.com>
2015-06-15 16:12:26 -0300

    agregando comienzos de test

--- test/test.js
+++ test/test.js
@@ -12,8 +12,45 @@ describe("MainCtrl", function() {
         controller = $controller('MainCtrl', { $scope: scope });
     }));
 
-    it("fills the foos", function() {
+    it("Prueba piloto", function() {
         expect(scope.test).toEqual('Viajes');
+        expect(scope.sort).toEqual('nombre');
+    });
+
+//Testeando sort y reverse    
+
+    it("Testear valor inicial del sort y reverse", function() {
+        expect(scope.sort).toEqual('nombre');
+        expect(scope.reverse).toEqual(false);
+    });
+
+    it("Testear changesort cuando el value es el mismo, por lo que deberia dejar el sort como esta y cambiar el reverse", function() {
+        
+        // De false que es el valor inicial cambia a true
+
+        expect(scope.sort).toEqual('nombre');        
+        expect(scope.reverse).toEqual(false);
+
+        scope.changeSort("nombre");
+
+        expect(scope.sort).toEqual('nombre');        
+        expect(scope.reverse).toEqual(true);
+
+        // De true que es el valor que quedo al realizar el cambio nuevamente cambia, esta vez a true
+        scope.changeSort("nombre");
+
+        expect(scope.sort).toEqual('nombre');        
+        expect(scope.reverse).toEqual(false);
+    });
+
+    it("Testear changesort cuando el value es distinto, por lo que deberia cambiar el sort y dejar el reverse como esta", function() {
+        expect(scope.sort).toEqual('nombre');        
+        expect(scope.reverse).toEqual(false);
+
+        scope.changeSort("fecha_inicio");
+
+        expect(scope.sort).toEqual('fecha_inicio');        
+        expect(scope.reverse).toEqual(false);
     });
 });
 
