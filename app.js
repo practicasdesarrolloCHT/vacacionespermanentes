a120bb0099f7ef0aa89ae15bb598e26f2ceeee4f
Tatiana <tatianamolinari90@gmail.com>
2015-07-04 03:54:54 -0300

    un intento mas de prueba de integracion continua

--- app.js
+++ app.js
@@ -14,19 +14,7 @@ if (process.env.NODE_ENV == undefined) {
   process.env.NODE_ENV = "test";
 }
 //
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-
-
-  console.log("sadwaeqwewq " + connection_string);
-
-  mongoose.connect(connection_string);
-}
-else { mongoose.connect(config.db[process.env.NODE_ENV]); }
+mongoose.connect(config.db["vacacionespermanentes"]);
 
 
 require('./models/Users');
